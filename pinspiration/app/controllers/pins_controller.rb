fd4d53668c46eb77760e1da93877d4c80761eb15
Kelly Austin <kaustly@gmail.com>
2015-11-13 10:00:49 -0500

    created_models_migrations_views_controller_for_pins

---
+++ pinspiration/app/controllers/pins_controller.rb
@@ -0,0 +1,5 @@
+class PinsController < ApplicationController
+
+
+
+end
---
+++ pinspiration/db/migrate/20151113145233_create_pins.rb
@@ -0,0 +1,9 @@
+class CreatePins < ActiveRecord::Migration
+  def change
+    create_table :pins do |t|
+      t.string :title
+      t.string :image_url
+      t.timestamps null: false
+    end
+  end
+end

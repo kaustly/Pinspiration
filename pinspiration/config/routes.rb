dadb98a2ba5bab3cfe5c4e313a95d9553edeccc9
Kelly Austin <kaustly@gmail.com>
2015-11-13 14:22:07 -0500

    YIKES

--- pinspiration/config/routes.rb
+++ pinspiration/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'users#index'
+  root 'pins#index'
 
   get 'pins', to: 'pins#index'
   # Example of regular route:
